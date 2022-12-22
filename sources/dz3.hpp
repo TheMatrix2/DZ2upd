1e735fad473c5935fa5b047b608b2ce6f900e3e2
thematrix <ermolovich.boris@yandex.ru>
2022-12-22 22:27:15 +0300

    заголовочный файл с описанием функций

---
+++ sources/dz3.hpp
@@ -0,0 +1,13 @@
+#ifndef DZ3_DZ3_H
+#define DZ3_DZ3_H
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+long int discriminant(int a, int b, int c);
+void add_to_file(const vector<char> m, const string name_file);
+
+#endif
